dda289035140ada3c7cd131404a15e8f7ff9cd63
Juan Enciso <juan.enciso@gmail.com>
2020-07-25 17:33:22 -0300

    Add show version

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+var pjson = require('../package.json');
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', {
-    title: 'Welcome to Contoso!',   
+    title: 'Welcome to Contoso!',
+    version: pjson.version,
   });
 });
 
